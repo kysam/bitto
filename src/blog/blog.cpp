7638d761b4fc049b1525c8064fa1bb2055cba0b2
Sam Le <samle@home.com>
2016-07-08 19:31:07 -0400

    Update

--- src/blog/blog.cpp
+++ src/blog/blog.cpp
@@ -19,12 +19,24 @@ void BLog::InitStream() {
   m_stream.open(BLOG_FILE, std::ios::binary);
 }
 
-void BLog::Log(const char* format, ...) {
+void BLog::Log(const char *format, ...) {
   if (!m_stream.is_open())
     InitStream();
 
   va_list args;
   va_start(args, format);
-  
 
+  char *str = ;
+  while(str != '\0') {
+    if (str != '%')
+      continue;
+
+    c++;
+    if (c == 'd') {
+      va_arg(args, int);
+    }
+    else if (c == 's') {
+      char* va_arg(args, char*);
+    }
+  }
 }
