0197e9c2f574a3ae6491cea0f7593a5384b65eef
Sam Le <kolonel81@gmail.com>
2016-07-08 19:38:54 -0400

    Update

--- src/blog/blog.cpp
+++ src/blog/blog.cpp
@@ -20,20 +20,12 @@ void BLog::InitStream() {
   m_stream.open(BLOG_FILE, std::ios::binary);
 }
 
-<<<<<<< HEAD
-void BLog::Log(const char* format, ...) {
-  if (!BLog::m_stream.is_open())
-=======
 void BLog::Log(const char *format, ...) {
   if (!m_stream.is_open())
->>>>>>> refs/remotes/origin/master
     InitStream();
 
   va_list args;
   va_start(args, format);
-<<<<<<< HEAD
-  
-=======
 
   char *str = ;
   while(str != '\0') {
@@ -48,5 +40,4 @@ void BLog::Log(const char *format, ...) {
       char* va_arg(args, char*);
     }
   }
->>>>>>> refs/remotes/origin/master
 }
